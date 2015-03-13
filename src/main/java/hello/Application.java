f3f99fda3c8a7fa437b8144aead001e796e708cf
MuPiehl <matthias.piehl@hypoport.de>
2015-03-13 02:50:39 +0100

    test commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) throws Throwable {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);// Test für Github
     }
 
 }
